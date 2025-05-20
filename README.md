a0a63482ecab386c0a4269475211f18820dd839a
Matthew Weeks <mweeks@Matthews-Mac-mini.local>
2025-05-19 20:12:08 -0700

    matt weeks updates

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ node version: 23.0.0
 from "root" run:        npm install  
 from "root/src" run:    npm run sass  
 from "root/src" run:    node app.js (open 2nd tab)  
-site listening on:      http://localhost:8100  
+site listening on:      http://localhost:8200  
 
 # Build / Deploy
 manually copy page source of respective pages to html files  
--- src/app.js
+++ src/app.js
@@ -7,11 +7,8 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('pages/home', { }))
 app.get('/about', (req, res) => res.render('pages/about', { }))
-//app.get('/pricing', (req, res) => res.render('pages/pricing', { }))
 app.get('/contact', (req, res) => res.render('pages/contact', { }))
 app.get('/portfolio', (req, res) => res.render('pages/portfolio', { }))
-app.get('/blog', (req, res) => res.render('pages/blog', { }))
-app.get('/post', (req, res) => res.render('pages/post', { }))
 
 app.listen(port, () => {
     console.log(`App listening on port: ${port}`)
